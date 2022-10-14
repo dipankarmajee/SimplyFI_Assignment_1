f954949f5d7d373b127b79a424be2e0b7022e051
DIPANKAR MAJEE <deep2dipankar@gmail.com>
2022-10-14 16:48:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
 import "./styles.css";
 
-// export default function App() {
-//   return (
-//     <div className="bg-container">
-//       <div className="box-card">
-//         <h2 className="title">
-//           HTML <span>&</span> CSS
-//         </h2>
-//       </div>
-//     </div>
-//   );
-// }
-
 const App = () => (
   <div className="bg-container">
     <div className="box-card">
